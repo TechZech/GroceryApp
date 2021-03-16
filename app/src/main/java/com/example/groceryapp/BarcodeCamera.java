cccc126053611e778d82d459fc3ae77d6b9c6919
TechZech <benzech7900@gmail.com>
2021-03-16 17:46:32 -0400

    created activity

---
+++ app/src/main/java/com/example/groceryapp/BarcodeCamera.java
@@ -0,0 +1,6 @@
+package com.example.groceryapp;
+
+
+// Activity that implements the Camera to be used to scan barcodes and send data to other activites on the item scanned
+public class BarcodeCamera {
+}
