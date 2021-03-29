e52c08be95d9ecc82b963af64b0215086a578d51
TrevorAlspach <60957308+TrevorAlspach@users.noreply.github.com>
2021-03-29 12:30:04 -0400

    Changed Colors, Beginning User Settings Fragment

--- UPC_Data/app/src/main/java/com/groceryapp/upcdata/MainActivity.java
+++ UPC_Data/app/src/main/java/com/groceryapp/upcdata/MainActivity.java
@@ -38,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
         setContentView(R.layout.activity_main);
 
         // BOTTOM NAVIGATION:
--- UPC_Data/app/src/main/java/com/groceryapp/upcdata/fragments/SettingsFragment.java
+++ UPC_Data/app/src/main/java/com/groceryapp/upcdata/fragments/SettingsFragment.java
@@ -2,16 +2,22 @@ package com.groceryapp.upcdata.fragments;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.OAuthCredential;
@@ -21,29 +27,44 @@ import com.groceryapp.upcdata.R;
 public class SettingsFragment extends Fragment {
 
     Activity context;
-
-    FirebaseAuth auth = FirebaseAuth.getInstance();
-    String email;
     Button btnLogOut;
+    ImageView ivProfile;
+    TextView tvEmail;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String email = user.getEmail();
+    Uri photoUrl = user.getPhotoUrl();
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        btnLogOut = view.findViewById(R.id.btnLogOut);
 
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LogOut();
-            }
-        });
-    }
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_settings, container, false);
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            btnLogOut = view.findViewById(R.id.btnLogOut);
+            ivProfile = view.findViewById(R.id.ivProfile);
+            tvEmail = view.findViewById(R.id.tvEmail);
+
+            tvEmail.setText(email);
+
+            btnLogOut.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LogOut();
+                }
+            });
+
+            Glide.with(this)
+                    .load(photoUrl)
+                    .placeholder(R.drawable.download)
+                    .transform(new FitCenter(), new RoundedCorners(25))
+                    .into(ivProfile);
+
+        }
 
     public void LogOut(){
         FirebaseAuth.getInstance().signOut();
