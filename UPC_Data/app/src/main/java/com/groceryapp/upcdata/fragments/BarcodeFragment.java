c570b2c9ff33753bb96d13351c313caa6afbe24c
TechZech <benzech7900@gmail.com>
2021-03-29 23:40:19 -0400

    deleted errors

--- UPC_Data/app/src/main/java/com/groceryapp/upcdata/fragments/BarcodeFragment.java
+++ UPC_Data/app/src/main/java/com/groceryapp/upcdata/fragments/BarcodeFragment.java
@@ -10,11 +10,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.groceryapp.upcdata.CaptureAct;
 import com.groceryapp.upcdata.R;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.io.IOException;
 
-public class BarcodeFragment extends Fragment {
+public class BarcodeFragment extends Fragment{
+
+    Button scanBtn;
 
     @Nullable
     @Override
@@ -25,7 +41,6 @@ public class BarcodeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
 
     }
 }
