5086da83f4fc2ef34d866416aa55f1fb41c447b5
TechZech <benzech7900@gmail.com>
2021-05-06 20:34:02 -0400

    Temp Friend Stuff

---
+++ UPC_Data/app/src/main/java/com/groceryapp/upcdata/fragments/FriendDetailFragment.java
@@ -0,0 +1,65 @@
+package com.groceryapp.upcdata.fragments;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.groceryapp.upcdata.DBHelper;
+import com.groceryapp.upcdata.R;
+
+public class FriendDetailFragment extends Fragment {
+
+    public static final String TAG = "FriendDetailFragment";
+    ImageView ivFriendPic;
+    TextView tvFriendName, tvFriendEmail;
+    Button btnRemoveFriend;
+
+    FirebaseUser user;
+    String email;
+    Uri userphotoUrl;
+    String Username;
+    DBHelper DBhelper;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_friend_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ivFriendPic = view.findViewById(R.id.ivFriendPic);
+        tvFriendEmail = view.findViewById(R.id.tvFriendEmail);
+        tvFriendName = view.findViewById(R.id.tvFriendName);
+        btnRemoveFriend = view.findViewById(R.id.btnRemoveFriend);
+/*
+        user = DBhelper.getUser();
+        email = user.getEmail();
+        userphotoUrl = user.getPhotoUrl();
+        Username = user.getDisplayName();
+        DBhelper = new DBHelper();
+*/
+        btnRemoveFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    // TODO: Write Code to remove a friend here
+            }
+        });
+
+    }
+
+}
